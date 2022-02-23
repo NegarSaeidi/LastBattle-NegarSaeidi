f5c8af1fd0dc5344be8f33d7fdb7fd5d64ba0e30
Negar <neg_r.saeidi95@yahoo.com>
2022-02-22 23:50:49 -0500

    shooting animation added

---
+++ Assets/Scripts/Player/Shooting.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+   
+    void Update()
+    {
+
+
+
+        if(Vector3.Magnitude(GetComponent<CharacterController>().velocity) == 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                GetComponent<Animator>().SetBool("ShootingIdle", true);
+            }
+            else
+            {
+                GetComponent<Animator>().SetBool("ShootingIdle", false);
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButton(0))
+            {
+                GetComponent<Animator>().SetBool("ShootingInMove", true);
+            }
+            else
+            {
+                GetComponent<Animator>().SetBool("ShootingInMove", false);
+            }
+        }
+       
+
+    }
+}
