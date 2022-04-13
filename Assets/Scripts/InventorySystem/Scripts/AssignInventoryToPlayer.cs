d6d1295a18b9e41f4c0d1e47f27ce1f07ffb53f6
Negar <neg_r.saeidi95@yahoo.com>
2022-04-13 12:49:12 -0400

    Fixed a bug with player animator

--- Assets/Scripts/InventorySystem/Scripts/AssignInventoryToPlayer.cs
+++ Assets/Scripts/InventorySystem/Scripts/AssignInventoryToPlayer.cs
@@ -49,6 +49,7 @@ public class AssignInventoryToPlayer : MonoBehaviour
                 }
                 else
                 {
+                   
                     MovementController.shieldCount++;
                    
                 }
